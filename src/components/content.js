4e580fd7f2c12e42b262f09077457292cc47c910
kvelraj_concord <kvelraj@concord.net>
2021-06-24 22:23:28 +0530

    onchenage

--- src/components/content.js
+++ src/components/content.js
@@ -2,10 +2,17 @@ import React, {Component} from 'react';
 
 class ContentComponent extends Component{
 
+  onHandleInput = (event) => {
+    console.log(event.target.value)
+  }
+
   render() {
     return (
        <div className="width-50-per">
          <h1>This is a Content Component</h1>
+         <div>
+           <input type="text" placeholder="Enter email id.." onChange={this.onHandleInput}/>
+         </div>
        </div>
     );
   }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,10 +2,27 @@ import React, {Component} from 'react';
 
 class SideBarComponent extends Component{
 
+  homePage(){
+    alert("HOME PAGE")
+  }
+  
+  inboxPage(){
+    alert("INBOX PAGE") 
+  }
+  
+  aboutPage(){
+    alert("ABOUT PAGE")
+  }
+
   render() {
     return (
        <div className="width-50-per">
          <h1>This is a Side Bar Component</h1>
+         <div>
+          <button className="btn" onClick={() => { this.homePage() }}>Home Page</button>
+          <button className="btn" onClick={() => { this.inboxPage() }}>Inbox Page</button>
+          <button className="btn" onClick={() => { this.aboutPage() }}>About Page</button>
+        </div>
        </div>
     );
   }
